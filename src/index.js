79d11aa664f872eaf79cf7251806f36aa6de2f4e
Tetiana85 <4215654@gmail.com>
2023-12-26 20:47:57 +0100

    1.1.2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import { App } from './components/App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOMClient.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
